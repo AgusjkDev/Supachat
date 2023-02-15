d2a811f4981aafef6a4c8ec0dfc98e7ebb27ab96
AgusjkDev <agusjkdev7@gmail.com>
2023-02-15 19:48:47 -0300

    Add query sanitization

--- data/regularExpressions.js
+++ data/regularExpressions.js
@@ -3,5 +3,6 @@ const username = /^[\w. ]{3,24}$/;
 const email =
     /^[\w!#$%&'*+\-/=?^`{|}~]+(?:\.[\w!#$%&'*+\-/=?^`{|}~]+)*@(?:[\w-]+\.)+([a-zA-Z]{2,})$/;
 const password = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?[\W_ ]).{8,32}$/;
+const sanitizeQuery = /[^\w. ]/g;
 
-export default { removeExtraSpaces, username, email, password };
+export default { removeExtraSpaces, username, email, password, sanitizeQuery };
--- hooks/useQuery.js
+++ hooks/useQuery.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 
+import { regex } from "data";
+
 export default function useSearch() {
     const [search, setSearch] = useState("");
     const [query, setQuery] = useState("");
 
     useEffect(() => {
-        const cleanSearch = search.trim();
+        const sanitizedQuery = search.replace(regex.sanitizeQuery, "");
 
-        setQuery(cleanSearch.length >= 3 ? cleanSearch : "");
+        setQuery(sanitizedQuery.length >= 3 ? sanitizedQuery : "");
     }, [search]);
 
     return { query, updateQuery: setSearch };
