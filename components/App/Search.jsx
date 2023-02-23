10302e921ac22ba9e3372e8d5e04878e8028546c
AgusjkDev <agusjkdev7@gmail.com>
2023-02-23 16:27:42 -0300

    Add status rendering in search results

--- components/App/Search.jsx
+++ components/App/Search.jsx
@@ -39,23 +39,35 @@ export default function Search() {
             </form>
 
             {results.length > 0 && (
-                <div className="absolute w-full border-b-[1px] border-background-700 lg:top-10 lg:w-4/5 lg:rounded-b-sm lg:border-x-[1px]">
-                    {results.map(({ id, username, profile_picture }) => (
+                <div className="absolute min-h-[calc(100vh-77px-53px)] w-full border-b-[1px] border-background-700 bg-background-900 lg:top-10 lg:min-h-0 lg:w-4/5 lg:rounded-b-sm lg:border-x-[1px]">
+                    {results.map(({ id, username, profile_picture, status }) => (
                         <button
                             key={id}
-                            className="flex w-full items-center gap-3.5 border-b-[1px] border-b-background-700 bg-background-900 p-4 transition-colors duration-300 last:border-none hover:bg-background-800 lg:p-3"
-                            onClick={() => {}}
+                            className="flex w-full justify-between border-b-[1px] border-b-background-700 p-4 pr-0 transition-colors duration-300 last:border-none hover:bg-background-800 lg:p-3.5 lg:pr-0"
                         >
-                            <div className="h-9 w-9">
-                                <ProfilePicture
-                                    username={username}
-                                    profile_picture={profile_picture}
-                                />
+                            <div className="flex gap-3">
+                                <div className="h-11 w-11">
+                                    <ProfilePicture
+                                        username={username}
+                                        profile_picture={profile_picture}
+                                    />
+                                </div>
+
+                                <div className="flex flex-col items-start justify-evenly">
+                                    <span className="text-sm font-medium text-secondary">
+                                        {username}
+                                    </span>
+
+                                    <span className="text-xs text-secondary-dark">{status}</span>
+                                </div>
                             </div>
 
-                            <span className="text-center text-sm font-medium text-secondary">
-                                {username}
-                            </span>
+                            <button
+                                aria-label="Opciones"
+                                className="group mr-1 grid h-11 w-11 place-items-center lg:hidden"
+                            >
+                                <Svg {...svgs.dots} />
+                            </button>
                         </button>
                     ))}
                 </div>
