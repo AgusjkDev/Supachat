84215c48550b28bbdf09aef6b35e6aa3d3a88d91
AgusjkDev <agusjkdev7@gmail.com>
2023-02-25 19:29:37 -0300

    Fix Automatic Static Optimization warning

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,12 @@
-import App from "next/app";
-
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
 
 MyApp.getInitialProps = async appContext => {
-    const appProps = await App.getInitialProps(appContext);
+    const { res } = appContext.ctx;
 
-    if (appContext.ctx.res?.statusCode === 404) {
-        appContext.ctx.res.writeHead(301, { Location: "/" });
-        appContext.ctx.res.end();
+    if (!res || res.statusCode !== 404) return;
 
-        return;
-    }
-
-    return { ...appProps };
+    appContext.ctx.res.writeHead(301, { Location: "/" });
+    appContext.ctx.res.end();
 };
